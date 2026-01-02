95de0d9acff48e50bae857ca640890550cc2aa14
mygithub05253 <kik328288@gmail.com>
2026-01-02 18:26:35 +0900

    feat: 전역 상태 관리 및 커스텀 훅 추가
    - Zustand UI 스토어 구현 (사이드바, 모바일 메뉴 상태)
    - use-sidebar 커스텀 훅 추가
    - ThemeProvider 추가 (next-themes)

---
+++ providers/theme-provider.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ReactNode } from 'react';
+
+interface ThemeProviderProps {
+  children: ReactNode;
+  [key: string]: any;
+}
+
+/**
+ * 테마 제공자 컴포넌트
+ * next-themes를 사용하여 다크모드 기능 제공
+ */
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
+}
